6409a28c42c3b8c9052934f51c636c3ff6aef396
James Hubert <jwhubert91@gmail.com>
2021-06-09 01:02:00 -0400

    Add next app

---
+++ pages/cars/index.js
@@ -0,0 +1,12 @@
+export default function CarsList() {
+  return (
+    <>
+      <h1>Cars List</h1>
+      <ul>
+        <li><a href='/cars/tesla'>Tesla</a></li>
+        <li><a href='/cars/lucid'>Lucid</a></li>
+        <li><a href='/cars/rivian'>Rivian</a></li>
+      </ul>      
+    </>
+  )
+}
